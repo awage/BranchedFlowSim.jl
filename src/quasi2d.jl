17fe2ca954cd99d2ffd1b1b1b62082c368ad054c
awage <>
2023-12-01 10:45:34 +0100

    File renaming

--- src/quasi2d.jl
+++ src/quasi2d.jl
@@ -8,6 +8,20 @@ export quasi2d_histogram_intensity
 export quasi2d_get_stats
 
 
+"""
+    sample_midpoints(a,b, n)
+
+Divide the interval [a,b] in n equally sized segments and return a Vector of
+midpoints of each segment.
+"""
+function sample_midpoints(a::Real, b::Real, n::Integer)::LinRange{Float64,Int64}
+    @assert n > 0
+    dx::Float64 = (b - a) / n
+    first = a + dx / 2
+    last = b - dx / 2
+    return LinRange(first, last, n)
+end
+
 
 function quasi2d_num_branches(num_rays, dt, ts, potential::AbstractPotential;
     return_rays=false, rays_span = (0,1))
