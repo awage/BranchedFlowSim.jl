c60b35f1e7570fc9ed8637400f10374cf3b81dbb
awage <>
2023-12-21 10:03:59 +0100

    finish animation program

--- scripts/periodic_std_pot/test_std_map.jl
+++ scripts/periodic_std_pot/test_std_map.jl
@@ -55,6 +55,33 @@ function interp_x(itp_x,t)
     return xit
 end
 
+function _produce_animation(T, itp_y, itp_p, xr, yr, image)
+# Animation begins
+    time = Observable(0.)
+    # the time series xs_1 and ys_1 are parametrized by the 
+    # $time. We use the interpolation function itp_ to get 
+    # intermediate time steps
+    xs_1 = @lift(mod.(interp_x(itp_y,$time),1))
+    ys_1 = @lift(interp_x(itp_p,$time))
+    fig = Figure(size = (2048,768))
+    gb = fig[1,1] = GridLayout()
+    ax1 = Axis(gb[1,1], ylabel = L"y", xlabel = L"p_y", title = @lift("t = $(round($time, digits = 1))"),)
+# Scatter plot for phase space.
+    scatter!(ax1, xs_1, ys_1, color = :blue, linewidth = 4)
+    ax2 = Axis(gb[1,2], ylabel = L"x", xlabel = L"y")
+# Density plot to track the trajectory
+    heatmap!(ax2, yr, xr, image)
+    lines!(ax2,[yr[1]; yr[end]],@lift([$time; $time]); color = :red)
+
+    framerate = 10
+    timestamps = range(0, T, step=0.01)
+    record(fig, "time_animation.mp4", timestamps;
+            framerate = framerate) do t
+        time[] = t
+    end
+
+end
+
 # Compute max lyap exp for a range of parameters
 ntraj = 10000;  a = 1; v0 = 3.7; dt = 1; T = 5; 
 d = @dict(ntraj, a, v0,  T, dt) # parametros
@@ -68,26 +95,4 @@ yr, xr, image = get_density_mat(Nx, Ny, d, itp_y)
 fig = heatmap(yr, xr, image)
 save("density_1.png",fig)
 
-
-# Animation begins
-time = Observable(0.)
-xs_1 = @lift(mod.(interp_x(itp_y,$time),1))
-ys_1 = @lift(interp_x(itp_p,$time))
-fig = Figure()
-gb = fig[1,1] = GridLayout()
-ax1 = Axis(gb[1,1], ylabel = L"y", xlabel = L"p_y", title = @lift("t = $(round($time, digits = 1))"),)
-# Scatter plot for phase space.
-scatter!(ax1, xs_1, ys_1, color = :blue, linewidth = 4)
-ax2 = Axis(gb[1,2], ylabel = L"x", xlabel = L"y")
-# Density plot to track the trajectory
-heatmap!(ax2, yr, xr, image)
-lines!(ax2,[yr[1]; yr[end]],@lift([$time; $time]); color = :red)
-
-framerate = 10
-timestamps = range(0, T, step=0.01)
-record(fig, "time_animation.mp4", timestamps;
-        framerate = framerate) do t
-    time[] = t
-end
-
-
+_produce_animation(T, itp_y, itp_p, xr, yr, image)
