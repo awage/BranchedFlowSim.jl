6657efdb0e3395aaddb8059493745783cb9712ca
awagemakers <awagemakers@debian-BULLSEYE-live-builder-AMD64>
2024-02-15 16:10:51 +0100

    stretch

--- scripts/figs_paper/calc_stretch.jl
+++ scripts/figs_paper/calc_stretch.jl
@@ -38,11 +38,11 @@ end
 function get_stretch_index(V; res = 500, a = 1, v0 = 1., dt = 0.01, Nt = 10000, prefix = "lyap")
     d = @dict(res,  a, v0,  Nt, dt, V) # parametros
     data, file = produce_or_load(
-        datadir("./storage"), # path
+        datadir("storage"), # path
         d, # container for parameter
         _get_stretch, # function
         prefix = prefix, # prefix for savename
-        force = true, # true for forcing sims
+        force = false, # true for forcing sims
         wsave_kwargs = (;compress = true)
     )
     @unpack λ = data
@@ -106,12 +106,44 @@ function compute_stretch_fermi(v0_range, num_rays, num_angles, dt)
     return c
 end
 
+function compute_stretch_cos(v0_range, d, num_rays, num_angles, dt)
+    gamma(x,y) = x - y/2*(sqrt(1+4*x/y) -1 )
+    angles = range(0, π/4, length = num_angles + 1)
+    angles = angles[1:end-1]
+    l_fer = zeros(length(v0_range),num_angles,2)
+    γ = zeros(length(v0_range), num_angles)
+    c = zeros(length(v0_range), num_angles)
+
+    for (j, θ) in enumerate(angles)
+        for (k,v0) in enumerate(v0_range)
+            # Correlated random pot 
+            correlation_scale = 0.1;
+            # t0 = v0^(-2/3)
+            Nt = round(Int, 40/dt)
+            lattice_a = 0.2; dot_radius = 0.2*0.25
+            softness = 0.2; II = rotation_matrix(θ)
+            V = RotatedPotential(θ,              
+                fermi_dot_lattice_cos_series(d,  
+                lattice_a, dot_radius, v0; softness))
+            s = savename("stretch_cos_n", @dict(d, θ))
+            α,β  = get_stretch_index(V; res = num_rays, a = 2, v0, dt, Nt, prefix = s)
+            l_fer[k,j,:] = [mean(α), mean(β)]*(v0^(-2/3)) 
+            @show a,b = [mean(α), mean(β)]*(v0^(-2/3))
+            @show γ[k,j] = gamma(a,b)
+            @show c[k,j] = gamma(a,b)/(v0^(-2/3))
+        end
+    end
+    return c
+end
+
 
 
 num_rays = 300; 
 num_angles = 20
 dt = 0.01; 
-v0_range = range(0.02, 0.2, step = 0.01); 
+v0_range = range(0.02, 0.4, step = 0.01); 
 c_r = compute_stretch_rand(v0_range, num_rays, dt)
 c_f = compute_stretch_fermi(v0_range, num_rays, num_angles, dt)
+c_c1 = compute_stretch_cos(v0_range, 1, num_rays, num_angles, dt)
+c_c6 = compute_stretch_cos(v0_range, 6, num_rays, num_angles, dt)
 
