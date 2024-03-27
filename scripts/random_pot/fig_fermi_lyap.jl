7ecd8b1c3c27367dd2dbb170e69ddba22dc04a6a
awage <>
2024-03-27 15:17:15 +0100

    fig fermi lyap

--- scripts/random_pot/fig_fermi_lyap.jl
+++ scripts/random_pot/fig_fermi_lyap.jl
@@ -79,7 +79,7 @@ function count_branches(y_ray, dy, ys; K = 1.)
             end
         end
     end
-    return br, hst
+    return br, sum(hst)/length(hst)
 end
 
 
@@ -89,8 +89,8 @@ function manifold_track(xs, ys, y_init, dyr, potential; K = 1.)
     dy = ys[2] - ys[1]
     num_rays = length(y_init)
     width = length(xs)
-    height = length(ys)
-    hst = zeros(height, width)
+    # height = length(ys)
+    hst = zeros(width)
     nb_br = zeros(width)
     ray_y = deepcopy(collect(y_init))
     ray_py = zeros(num_rays)
@@ -102,7 +102,7 @@ function manifold_track(xs, ys, y_init, dyr, potential; K = 1.)
         ray_y .+= dt .* ray_py
         x += dt
         while xi <= length(xs) &&  xs[xi] <= x 
-            nb_br[xi], hst[:,xi] = count_branches(ray_y, dyr, ys)
+            nb_br[xi], hst[xi] = count_branches(ray_y, dyr, ys)
             xi += 1
         end
     end
@@ -123,13 +123,37 @@ xs = range(0,15., step = dt)
 
 
 a = 0.2; dot_radius = 0.2*0.25; softness = 0.2; θ_range = range(0,π/4, length = n_avg) 
-nb_br_v = Vector{Vector{Float64}}()
+hst_v_all = Vector{Vector{Float64}}()
+hst_v_pos = Vector{Vector{Float64}}()
+hst_v_z = Vector{Vector{Float64}}()
+nb_v_all = Vector{Vector{Float64}}()
+nb_v_pos = Vector{Vector{Float64}}()
+nb_v_z = Vector{Vector{Float64}}()
 Threads.@threads for j = 1:n_avg
     V = LatticePotential(a*rotation_matrix(θ_range[j]), dot_radius, v0; softness=softness)
     dat = get_branch_number(v0, V, y_init, xs, num_rays, j, K, threshold, T; prefix = "fermi_br_lyap") 
-    @unpack nb_br_pos = dat
-    push!(nb_br_v, nb_br_pos)
+    @unpack hst_all, hst_pos, hst_z = dat
+    @unpack nb_br_all, nb_br_pos, nb_br_z = dat
+
+    push!(hst_v_all, hst_all)
+    push!(hst_v_z, hst_z)
+    push!(hst_v_pos, hst_pos)
+    push!(nb_v_all, nb_br_all)
+    push!(nb_v_z, nb_br_z)
+    push!(nb_v_pos, nb_br_pos)
 end
 
-# s = "fermi_lyap_branch.png"
-# save(plotsdir(s),fig)
+f = Figure(); ax = Axis(f[1,1])
+lines!(ax, xs, mean(hst_v_pos, dims = 1)[1])
+lines!(ax, xs, mean(hst_v_z, dims = 1)[1]; color = :red)
+lines!(ax, xs, mean(hst_v_all, dims = 1)[1]; color = :black)
+s = "fermi_lyap_branch_hst.png"
+save(plotsdir(s),f)
+
+
+f = Figure(); ax = Axis(f[1,1])
+lines!(ax, xs, mean(nb_v_pos, dims = 1)[1])
+lines!(ax, xs, mean(nb_v_z, dims = 1)[1]; color = :red)
+lines!(ax, xs, mean(nb_v_all, dims = 1)[1]; color = :black)
+s = "fermi_lyap_branch_num.png"
+save(plotsdir(s),f)
